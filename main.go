ea2f2e57d82f4a0c13c36955c8ede22011c71691
akilynk <aki@lynker.app>
2023-10-27 00:55:27 -0400

    support hash from stdin

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"crypto/sha256"
 	"crypto/sha512"
 	"fmt"
+	"io"
 	"os"
 )
+
+const version = "2.0.0"
 
 type hashFunc func([]byte) string
 
@@ -19,22 +22,45 @@ var method2Func map[string]hashFunc = map[string]hashFunc{
 }
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) < 2 {
 		help()
 		return
 	}
 	method := os.Args[1]
-	content := []byte(os.Args[2])
+	if method == "-v" || method == "--version" {
+		fmt.Println(version)
+		return
+	}
 	hashF, ok := method2Func[method]
 	if !ok {
 		help()
 		return
 	}
+	var content []byte
+	var err error
+	if len(os.Args) == 3 {
+		content = []byte(os.Args[2])
+	} else {
+		content, err = io.ReadAll(os.Stdin)
+		if err != nil {
+			fmt.Println("failed to read stdin. error:", err)
+			return
+		}
+	}
 	fmt.Println(hashF(content))
 }
 
 func help() {
-	info := "hashsum [md5|sha1|sha256|sha512] content-to-hash"
+	info := `hashsum [md5|sha1|sha256|sha512] content-to-hash
+  Hash content. If there is no content provided, it will read input from stdin.
+  Example:
+  - hashsum md5 "hello world"
+  - echo "hello world" | hashsum md5
+  - cat hello.txt | hashsum md5
+
+hashsum -v|--version
+  Show hashsum current version
+`
 	fmt.Println(info)
 }
 
